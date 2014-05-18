8255f51f7d298fc7b7e3daf8f6d265ae2da07a2f
sugama <k-sugama@skyarc.co.jp>
2014-05-19 01:54:20 +0900

    initial commit

---
+++ MTDataAPIYetAnother.js
@@ -0,0 +1,329 @@
+/**
+ * MTDataAPIYetAnother.js - Yet another MT DataAPI SDK using jQuery
+ *
+ * SYNOPSIS
+ *
+ * var api = new MT.MTDataAPIYetAnother({
+ *     baseUrl:"http://192.168.56.101:5000/mt-data-api.cgi",
+ *     enableJsonp:false
+ * });
+ * 
+ * api.listCategoryStats(siteId, function(json){...});
+ */
+;(function($) {
+    
+    /**
+     * Default params
+     */
+    var default_params = {
+        baseUrl: undefined,
+        enableJsonp: true
+    };
+    
+    /**
+     * Constructor
+     * @param {Object} params that overrides the default
+     */
+    var Class = function(params) {
+        this.params = $.extend(default_params, params, {});
+    };
+    
+    /**
+     * Emulation of official SDK's API with JSONP support
+     * @param {Number} Site ID
+     * @param {Object} Additional Parameters
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.getEntry = function(siteId, entryId, params, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/entries/" + entryId;
+        var query = {
+            fields: params.fields
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            cb && cb(data);
+        });
+    };
+    
+    /**
+     * Category list with numbers of entries
+     * TODO Better implement as server side endpoint for it.
+     * @param {Number} Site ID
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.listCategoryStats = function(siteId, cb) {
+        
+        var api = this;
+        
+        return api.listCategories(siteId, {fields:'basename,label'}, function(data) {
+            
+            var dfd = $.Deferred();
+            var deferredObjects = [];
+            
+            for (var idx in data.items) {
+                var name = data.items[idx].basename;
+                var path = "/v1/sites/1/entries";
+                var query = {
+                    category: name,
+                    fields: 'DUMMY', // it works for now
+                    limit: 1000
+                };
+                var xhr = api.getJSON(path, query);
+                deferredObjects.push(xhr);
+            }
+            
+            $.when.apply(null, deferredObjects).done(function() {
+                var items = Array.prototype.slice.call(arguments, 0).map(
+                                                                function(v, i) {
+                    return {
+                        totalResults: v[0].totalResults,
+                        basename: data.items[i].label
+                    };
+                });
+                var stat = {
+                    totalResults: items.length,
+                    items: items
+                };
+                dfd.resolve();
+                cb && cb(stat);
+            });
+            
+            return dfd.promise();
+        });
+    };
+    
+    /**
+     * Category list
+     * @param {Number} Site ID
+     * @param {Object} Additional Parameters
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.listCategories = function(siteId, params, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/categories";
+        var query = {
+            fields: params.fields
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            cb && cb(data);
+        });
+    };
+    
+    /**
+     * Tag list
+     * TODO Better implement as server side endpoint for it.
+     * @param {Number} Site ID
+     * @param {Number} Max rank number
+     * @param {Object} Additional Parameters
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.listTags = function(siteId, maxRank, params, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/entries";
+        var query = {
+            fields: 'tags',
+            date_type: 'authored_on',
+            after: yyyymmdd(monthAgo(params.limit || 5)),
+            limit: 1000
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            var total = {};
+            for (var idx in data.items) {
+                var tags = data.items[idx].tags;
+                for (idx2 in data.items[idx].tags) {
+                    var name = data.items[idx].tags[idx2];
+                    total[name] = total[name] || 0;
+                    total[name]++;
+                }
+            }
+            var items = Object.keys(total).map(function(k) {
+                return {
+                    name: k,
+                    totalResults: total[k],
+                };
+            });
+            setRanks(items, maxRank);
+            var stat = {
+                totalResults: items.length,
+                items: items
+            };
+            cb && cb(stat);
+        });
+    };
+    
+    /**
+     * Recent Entries
+     * @param {Number} Site ID
+     * @param {Object} Additional Parameters
+     * @param {Function} callback
+     */
+    Class.prototype.listRecentEntries = function(siteId, params, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/entries";
+        var query = {
+            fields: params.fields,
+            limit: params.limit,
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            cb && cb(data);
+        });
+    };
+    
+    /**
+     * Month list with numbers of entries
+     * TODO Better implement as server side endpoint for it.
+     * @param {Number} Site ID
+     * @param {Object} Additional Parameters
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.listMonthlyEntryCounts = function(siteId, params, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/entries";
+        var query = {
+            fields: 'createdDate',
+            date_type: 'authored_on',
+            after: yyyymmdd(monthAgo(params.limit - 1)),
+            limit: 1000
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            var total = {};
+            for (var idx in data.items) {
+                var ym = data.items[idx].createdDate.substr(0, 7);
+                total[ym] = total[ym] || 0;
+                total[ym]++;
+            }
+            var items = Object.keys(total).map(function(k) {
+                return {
+                    year: parseInt(k.substr(0, 4)),
+                    month: parseInt(k.substr(5, 2)),
+                    totalResults: total[k]
+                };
+            });
+            var stat = {
+                totalResults: items.length,
+                items: items
+            };
+            cb && cb(stat);
+        });
+    };
+    
+    /**
+     * Number of entries for given month
+     * @param {Number} Site ID
+     * @param {Number} year like 2014
+     * @param {Number} month number of 1 to 12
+     * @param {Function} cb
+     * @return {jqXhr}
+     */
+    Class.prototype.monthlyEntryCount = function(siteId, year, month, cb) {
+        
+        var path = "/v1/sites/" + siteId + "/entries";
+        var query = {
+            fields: 'DUMMY', // it works for now
+            date_type: 'authored_on',
+            from: yyyymmdd(new Date(year, month - 1, 1)),
+            to: yyyymmdd(new Date(year, month, 0)),
+            limit: 1000
+        };
+        
+        return this.getJSON(path, query, function(data) {
+            cb && cb(data.totalResults);
+        });
+    };
+    
+    /**
+     * JSON request
+     * @param {Number} Site ID
+     * @param {Object} Options
+     * @param {Function} callback
+     * @return {jqXhr}
+     */
+    Class.prototype.getJSON = function(path, opt, cb) {
+        
+        var query = $.param(opt);
+        
+        return $.ajax({
+            type: 'GET',
+            url: this.params.baseUrl + path + (query ? "?" + query : ""),
+            dataType: this.params.enableJsonp ? 'jsonp' : 'json',
+            success: function(data) {
+                cb && cb(data);
+            }
+        });
+    };
+    
+    window.MT = window.MT || {};
+    window.MT.MTDataAPIYetAnother = window.MT.MTDataAPIYetAnother || Class;
+    
+    /**
+     * Date formatter for DataAPIExtendSearch plugin API
+     * @param {Date} Source date object
+     * @return {Object}
+     */
+    function yyyymmdd(d) {
+        return [
+            d.getFullYear(),
+            ('0' + (d.getMonth() + 1)).slice(-2),
+            ('0' + d.getDate()).slice(-2)
+        ].join('-');
+    }
+    
+    /**
+     * Generate month interval by number of months
+     * @param {Number} Offset Number of months
+     * @return {Date}
+     */
+    function monthAgo(offset) {
+        var today = new Date();
+        var y = today.getFullYear();
+        var m = today.getMonth();
+        yearPast = y + Math.floor((m - offset) / 12);
+        monthPast = (m - offset) % 12;
+        while (monthPast < 0) {
+            monthPast += 12;
+        }
+        return new Date(yearPast, monthPast, 1);
+    }
+    
+    /**
+     * Set tag cloud ranks for each entries
+     * @param {Object} Entry items
+     * @param {Number} Max number for rank
+     * @return void
+     */
+    function setRanks(items, maxRank) {
+        var nums = items.map(function(i) {
+            return i.totalResults;
+        });
+        maxRank--;
+        
+        var maxNum = Math.max.apply(null, nums);
+        var minNum = Math.min.apply(null, nums);
+        
+        if (maxNum == minNum) {
+            items.map(function(i) {
+                i.rank = parseInt(maxRank / 2);
+            });
+            return;
+        }
+        
+        var sqrtMax = Math.sqrt(maxNum);
+        var sqrtMin = Math.sqrt(minNum);
+        var sqrtDiff = sqrtMax - sqrtMin;
+        items.map(function(i) {
+            var sqrt = Math.sqrt(i.totalResults);
+            i.rank = parseInt((sqrt - sqrtMin) / sqrtDiff * maxRank + 0.5) + 1;
+        });
+    }
+    
+})(jQuery);
---
+++ test/api-mock.psgi
@@ -0,0 +1,250 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+use JSON;
+use Plack::Builder;
+use Plack::Request;
+use Data::Dumper;
+use List::MoreUtils;
+
+my @category_table = generate_categories(5);
+my @tag_table = generate_tags(10);
+my @entry_table = generate_entries(500);
+
+my $app = sub {
+    my $env = shift;
+    my $req = Plack::Request->new($env);
+    my $items;
+    
+    if ($req->path_info =~ qr'^/v1/sites/1/entries/(\d+)$') {
+        return res_single(entry($req, $1));
+    }
+    
+    if ($req->path_info =~ qr'^/v1/sites/1/entries$') {
+        return res_multi(entries($req));
+    }
+    
+    if ($req->path_info =~ qr'^/v1/sites/1/categories$') {
+        return res_multi(categories($req));
+    }
+    
+    return res_single(error(404, 'Unknown endpoint'));
+};
+
+builder {
+    enable 'JSONP';
+    $app;
+};
+
+sub res_error {
+    return [
+        200,
+        [ "Content-Type", "application/json" ],
+        [
+            encode_json(
+                {error => {"code" => $_[0], "message" => $_[1]}}),
+        ],
+    ];
+}
+
+sub res_single {
+    my $items = shift;
+    return [
+        200,
+        [ "Content-Type", "application/json" ],
+        [
+            encode_json($items),
+        ],
+    ];
+}
+
+sub res_multi {
+    my $items = shift;
+    return [
+        200,
+        [ "Content-Type", "application/json" ],
+        [
+            encode_json({
+                items => $items,
+                totalResults => scalar @$items,
+            }),
+        ],
+    ];
+}
+
+sub generate_categories {
+    return generate_word_list(shift,
+                            ['製品', '研究', 'お知らせ', 'News']);
+}
+
+sub generate_tags {
+    return generate_word_list(shift,
+                            ['MovableType', 'jQuery', 'Perl', 'PHP', '技術全般']);
+}
+
+sub generate_word_list {
+    my ($size, $words) = @_;
+    my %count;
+    my @ret;
+    
+    while (scalar @ret < $size) {
+        my $key = $words->[scalar @ret % scalar @$words];
+        push(@ret, $key . ($count{$key} || ''));
+        $count{$key} = ($count{$key} || 1) + 1;
+    }
+    return @ret;
+}
+
+sub generate_entries {
+    my $size = shift;
+    my %template = (
+        id => 1,
+        blog => {
+            id => '1'
+        },
+        author => {
+            userpicUrl => undef,
+            displayName => 'skr',
+        },
+        title => '記事',
+        body => '<p>ほげほげ</p>',
+        excerpt => 'ほげほげ...',
+        keywords => '',
+        allowTrackbacks => JSON::true,
+        trackbacks => [],
+        trackbackCount => '0',
+        categories => [],
+        commentCount => '0',
+        tags => [],
+        basename => 'article',
+        assets => [],
+        pingsSentUrl => [],
+        class => 'entry',
+        more => '',
+        status => 'Publish',
+        updatable => JSON::false,
+        allowComments => JSON::true,
+        comments => [],
+        permalink => 'http://192.168.56.101/2014/05/article.html',
+    );
+    
+    my @entries;
+    
+    for my $id (1..$size) {
+        my $date = randomDate(86400 * 500);
+        my %override = (
+            id => $id,
+            title => '記事-'. $id,
+            modifiedDate => $date,
+            createdDate => $date,
+            date => $date,
+            categories => randomCategory(),
+            tags => randomTag(),
+        );
+        my %unique = (%template, %override);
+        
+        push(@entries, \%unique);
+    }
+    
+    @entries = sort {$b->{createdDate} cmp $a->{createdDate}} @entries;
+    
+    return @entries;
+}
+
+sub error {
+    return {error => {'code' => $_[0], 'message' => $_[1]}};
+}
+
+sub entry {
+    my ($req, $id) = @_;
+    my @ret = grep {$_->{id} == $id} @entry_table;
+    return $ret[0] || error(404, 'Entry not found');
+}
+
+sub entries {
+    my ($req) = @_;
+    
+    my @filters;
+    
+    if (my $val = $req->param('after')) {
+        push(@filters, sub {
+            ($_->{createdDate} =~ qr{(.+)T})[0] gt $val
+        });
+    }
+    
+    if (my $val = $req->param('category')) {
+        utf8::decode($val);
+        push(@filters, sub {
+            grep {$_ && $_ eq $val} @{$_->{categories}}
+        });
+    }
+    
+    if (my $val = $req->param('tag')) {
+        utf8::decode($val);
+        push(@filters, sub {
+            grep {$_ && $_ eq $val} @{$_->{tags}}
+        });
+    }
+    
+    my @ret = @entry_table;
+    
+    @ret = grep {
+        my $hit = 1;
+        for my $cb (@filters) {
+            ($hit = $cb->($_)) || last;
+        }
+        $hit;
+    } @ret;
+    
+    if (my $val = $req->param('offset')) {
+        splice(@ret, 0, $val);
+    }
+    
+    if (my $val = $req->param('limit')) {
+        splice(@ret, $val - 1);
+    }
+    
+    if (scalar (my @fields = split(',', $req->param('fields') || ''))) {
+        @ret = map {
+            my $reduced = {};
+            for my $f (@fields) {
+                $reduced->{$f} = $_->{$f} if exists $_->{$f};
+            }
+            $reduced;
+        } @ret;
+    }
+    
+    return \@ret;
+}
+
+sub randomDate {
+    my $scope = shift;
+    my ($sec, $min, $hour, $mday, $mon, $year, undef, undef, undef) =
+                                            localtime(time - int(rand $scope));
+    return sprintf(qq{%04d-%02d-%02dT%02d:%02d:%02d},
+                            $year + 1900, $mon + 1, $mday, $hour, $min, $sec);
+}
+
+sub randomCategory {
+    my @ret;
+    for (0 .. int(rand(scalar @category_table))) {
+        push(@ret, $category_table[int(rand(scalar @category_table)) - 1]);
+    }
+    return [List::MoreUtils::uniq @ret];
+}
+
+sub randomTag {
+    my @ret;
+    for (0 .. int(rand(scalar @tag_table))) {
+        push(@ret, $tag_table[int(rand(scalar @tag_table)) - 1]);
+    }
+    return [List::MoreUtils::uniq @ret];
+}
+
+sub categories {
+    return [map {{
+        basename => $_,
+        label => $_,
+    }} @category_table];
+}
