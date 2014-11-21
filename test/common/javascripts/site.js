52e0787c6ffbcea87265e50cfa97024322460032
skyarc <skyarc@ip-10-0-0-20.ap-northeast-1.compute.internal>
2014-11-21 05:56:08 -0500

    true

--- test/common/javascripts/site.js
+++ test/common/javascripts/site.js
@@ -5,7 +5,7 @@ $(function(){
         //baseUrl:"http://127.0.0.1:5000",
         baseUrl:"http://mtddc.skyarc.org/cgi-bin/mt/mt-data-api.cgi",
         internalLimit: 1000,
-	enableJsonp: false
+	enableJsonp: true
     });
     
     var routes = {
